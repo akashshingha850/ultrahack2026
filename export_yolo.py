f8be98f7bc158b6b4ed1ba9a4eb93829fce89b00
Akash Shingha <akashshingha850@gmail.com>
2026-06-07 18:03:11 +0300

    Update RTSP stream configuration in config.yaml; add YOLO test script for detection on RTSP stream.

---
+++ export_yolo.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+
+# Load a YOLO26n PyTorch model
+model = YOLO("fireman_yolo26s_taufiq.pt")
+
+# Export the model to TensorRT
+model.export(format="engine", half=True, )  # creates 'yolo26n.engine'
+
+# # Load the exported TensorRT model
+# trt_model = YOLO("yolo26n.engine")
+
+# # Run inference
+# results = trt_model("https://ultralytics.com/images/bus.jpg")
